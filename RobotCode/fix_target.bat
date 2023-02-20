0a57522235e0261c9d36d91271866f5805ece0d2
jsimpso81 <60305400+jsimpso81@users.noreply.github.com>
2023-02-20 13:48:51 -0500

    update lvproj fix target batch file -- added compare

--- RobotCode/fix_target.bat
+++ RobotCode/fix_target.bat
@@ -1,5 +1,13 @@
+@echo off
+
+REM ---- Save the previous file for comparison
 copy 2023RobotCode.lvproj 2023RobotCode.lvproj.bak
 
+REM ---- Replace the the bad target
 replace.vbs 2023RobotCode.lvproj 7AAE 76F2
 
+REM ---- show user what was changed.
+fc 2023RobotCode.lvproj.bak 2023RobotCode.lvproj
+
+REM ---- all done
 pause done
