9d1b4d1d67b45ec4329ed20df01f32993fdf818b
Frobotics4150 <frobotics4150@gmail.com>
2023-07-20 18:59:37 -0400

    update fix_target for new project name

--- ZoomDrive/fix_target.bat
+++ ZoomDrive/fix_target.bat
@@ -1,13 +1,13 @@
 @echo off
 
 REM ---- Save the previous file for comparison
-copy 2023RobotCode.lvproj 2023RobotCode.lvproj.bak
+copy ZoomCode.lvproj ZoomCode.lvproj.bak
 
 REM ---- Replace the the bad target
-replace.vbs 2023RobotCode.lvproj 7AAE 76F2
+replace.vbs ZoomCode.lvproj 7AAE 76F2
 
 REM ---- show user what was changed.
-fc 2023RobotCode.lvproj.bak 2023RobotCode.lvproj
+fc ZoomCode.lvproj.bak ZoomCode.lvproj
 
 REM ---- all done
 pause done
